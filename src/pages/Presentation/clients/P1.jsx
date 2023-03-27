5be4a028599a0a41c3a7d4539ac71b4235cdf394
Nada-omri <118102324+Nada-omri@users.noreply.github.com>
2023-03-27 04:17:15 +0100

    J'ai corrigé la pagination pour qu'elle puisse permettre de revenir aux avis précédents.

--- src/pages/Presentation/clients/P1.jsx
+++ src/pages/Presentation/clients/P1.jsx
@@ -8,12 +8,11 @@ import im4 from './pic.png'
 import im9 from './pic3.png' 
 import im7 from './pic2.png'
 import { useState } from 'react'
-import { color, style } from '@mui/system'
 
 export function P1() {
   const [currentpage, setCurrentpage] = useState(1)
-
-  const handleDotClick = () => {
+ 
+const handleDotClick = () => {
     if(currentpage==1){
     setCurrentpage(2)}
     else if (currentpage===2){
@@ -23,7 +22,7 @@ export function P1() {
 
   if (currentpage==1) {
     return (
-      <div>
+      <div className="av1">
         <h1>Ce que les clients disent de nous</h1>
         <hr className='light'/>
         <div className='container'>
@@ -56,14 +55,14 @@ export function P1() {
         </div>
         <div className='circles'>
           <span className='dot1'></span>
-          <span className='dot2'onClick={handleDotClick}></span>
-          <span className='dot3'></span>
+          <span className='dot2'onClick={handleDotClick} ></span>
+          <span className='dot3'onClick={()=>{setCurrentpage(3)}}></span>
         </div>
       </div>
     )
   } else if (currentpage==2) {
     return (
-      <div>
+      <div className="av1">
         <h1>Ce que les clients disent de nous</h1>
         <hr className='light'/>
       <div className='container'>
@@ -90,7 +89,7 @@ export function P1() {
 
     </div>
      <div className='circles'>
-    <span className='dot1'></span>
+    <span className='dot1' onClick={()=>{setCurrentpage(1)}}></span>
     <span className='dot2'></span>
     <span className='dot3'onClick={handleDotClick}></span>
     </div>
@@ -98,16 +97,16 @@ export function P1() {
   )}
   else if (currentpage==3) {
     return (
-      <div>
+      <div className="av1">
         <h1>Ce que les clients disent de nous</h1>
         <hr className='light'/>
       <div className='container'>
       <div className='child1'>
-        <div className='appostrophe'><img width={90} src ={im3} /></div>
+        <div className='appostrophe'><img width={70} src ={im3} /></div>
         <div className='text'> <p id='avis'> Je recommande fortement ce site à tous ceux 
         qui recherchent un service fiable et de qualité.</p></div>
         <div className='imname1'>
-        <div className='im'><img width={90} src ={im7} /></div>
+        <div className='im'><img width={70} src ={im7} /></div>
           <div className='name'><p id='nom'>Foulen.f</p>
           <p id='p'><strong>- Constantine Constantinople -</strong> </p></div>
           
@@ -117,7 +116,7 @@ export function P1() {
       <div className='appostrophe'><img width={90} src ={im3} /></div>
       <div className='text'> <p id="avis">J'ai récemment utilisé ce site web pour la première fois et j'ai été impressionné par la qualité de ses services. La navigation sur le site est facile et intuitive, et j'ai pu trouver rapidement ce que je cherchais. La livraison de ma commande a été rapide et sans problèmes.." </p></div>
         <div className='imname'>
-          <div className='im'><img width={90} src ={im9} /></div>
+          <div className='im'><img width={90}  src ={im9} /></div>
           <div className='name'> <p id='nom'>Foulena.f</p>
           <p id='p'><strong>- Constantine Constantinople -</strong> </p></div>
         </div>
@@ -125,16 +124,10 @@ export function P1() {
 
     </div>
      <div className='circles'>
-    <span className='dot1' onClick={handleDotClick} ></span>
-    <span className='dot2'></span>
+    <span className='dot1' onClick={handleDotClick } ></span>
+    <span className='dot2' onClick={()=>{setCurrentpage(2)}}></span>
     <span className='dot3'></span>
     </div>
     </div>
   )}
   }
-
-
-
-
-
-
