2d67b97cc36273484969578d0b28d4fba26f5a65
ahmed-mrabet <121690139+ahmed-mrabet@users.noreply.github.com>
2023-03-28 03:14:45 +0100

    services page
    almost done

--- src/pages/Services/Services.jsx
+++ src/pages/Services/Services.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 import "./Services.css";
-
+import {Box} from "@mui/material"
+import Card1 from "./services-components/card1";
+import Card2 from "./services-components/card2";
+import Card3 from "./services-components/card3";
 function Services() {
-  return <div>Services1</div>;
-}
+  return(
+    <Box>
+    <Card1></Card1>
+    <Card2></Card2>
+    <Card3></Card3>
+    </Box>
+      )
 
-export default Services;
+}
+export default Services
