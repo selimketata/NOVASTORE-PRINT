3f4fe88b1ae17a10be8faac2d8ef9d3752dc18a9
ahmed-mrabet <121690139+ahmed-mrabet@users.noreply.github.com>
2023-03-28 17:49:36 +0100

    Update Services.jsx

--- src/pages/Services/Services.jsx
+++ src/pages/Services/Services.jsx
@@ -4,12 +4,16 @@ import {Box} from "@mui/material"
 import Card1 from "./components/card1";
 import Card2 from "./components/card2";
 import Card3 from "./components/card3";
+import Navbar from"../../components/Navbar"
+import Footer from"../../components/Footer"
 function Services() {
   return(
     <Box>
+    <Navbar></Navbar>
     <Card1></Card1>
     <Card2></Card2>
     <Card3></Card3>
+    <Footer></Footer>
     </Box>
       )
 
