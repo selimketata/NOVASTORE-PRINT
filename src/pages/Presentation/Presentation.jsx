e03320efb57888d812b012784369f25a397cdd9d
Nada-omri <118102324+Nada-omri@users.noreply.github.com>
2023-04-13 17:28:13 +0100

    correction de quelques détails

--- src/pages/Presentation/Presentation.jsx
+++ src/pages/Presentation/Presentation.jsx
@@ -1,21 +1,10 @@
-import P1 from '../Presentation/clients/P1';
-import { useState} from 'react';
-import {  getUsers } from '../Presentation/clients/Users';
+
+import P2 from './clients/P2';
 
 function Presentation() {
-  const [page, setPage] = useState(1);
-  const [limit, setLimit] = useState(2);
-
   return (
     <div className="App">
-      <div >
-        <P1 Users={getUsers(page, limit)} />
-      </div>
-      <div className="circles">
-      <span className="dot1" onClick={() => { setPage(1)}}></span>
-      <span className="dot1" onClick={() => { setPage(2) }}></span>
-        <span className="dot3" onClick={() => { setPage(3); }}></span>
-      </div>
+    <P2/>
     </div>
   );
 }
---
+++ src/pages/Presentation/clients/P2.jsx
@@ -0,0 +1,23 @@
+import P1 from './P1'
+import { useState} from 'react';
+import {  getUsers } from './Users';
+
+function P2() {
+  const [page, setPage] = useState(1);
+  const [limit, setLimit] = useState(2);
+
+  return (
+    <div className="App">
+      <div >
+        <P1 Users={getUsers(page, limit)} />
+      </div>
+      <div className="circles">
+      <span className="dot1" onClick={() => { setPage(1)}}></span>
+      <span className="dot1" onClick={() => { setPage(2) }}></span>
+        <span className="dot3" onClick={() => { setPage(3); }}></span>
+      </div>
+    </div>
+  );
+}
+
+export default P2;
