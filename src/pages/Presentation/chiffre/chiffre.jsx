afdab3149ac2f891083661c5b76ee8bbbe70366e
kalleliyet <mohamedazizkallel2001@gmail.com>
2023-04-16 19:03:22 +0200

    counting fixed

--- src/pages/Presentation/chiffre/chiffre.jsx
+++ src/pages/Presentation/chiffre/chiffre.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import CountUp,{useCountUp} from  'react-countup'
+import React,{useState,useEffect,useRef} from 'react';
+import { useInView } from 'react-intersection-observer';
+import CountUp from  'react-countup'
 import "./chiffre.css";
 
 
@@ -10,48 +11,48 @@ import "./chiffre.css";
 
 function Chiffre({number,adding}){
 
-    /*const {ref:myRef ,inView:myElementIsVisible }=useInView();
+    const {ref:myRef ,inView:myElementIsVisible }=useInView();
     
-
-
-
-        
-
-
+    let visibleOnce=useRef(false)
+    
+    const [styles,setStyles]=useState({count:{
+      display:"contents"
+    },noCount:{
+      display:"none"
+    }
+    })
+    
+    
+    /*setStyles({count:{
+      display:"contents"
+    },noCount:{
+      display:"none"
+    }
+    })
+*/
     
     useEffect(()=>{
         if (myElementIsVisible){
+          visibleOnce.current=true
         }
-    },[myElementIsVisible,])
+        if(visibleOnce.current)
+        setTimeout(()=>{
+          setStyles({count:{
+            display:"none"
+          },noCount:{
+            display:"contents"
+          }
+          })
+          console.log("visible")
+        },3000)
+    },[myElementIsVisible])
 
 
-
-
-    
-    return(
-        <div id="content" ref={myRef}>
-            
-            <span className="chiffre">{adding}</span>
-
-                  
-        </div>
-
-
-    
-    )*/
-
-    useCountUp({
-        ref: 'counter',
-        end: 100,
-        duration:10,
-        enableScrollSpy: true,
-        scrollSpyDelay: 0,
-      });
     
       return (
         <div id="content">
    
-          <span className="chiffre" ><CountUp  end={number} enableScrollSpy duration={5} />{adding}</span>
+          <span className="chiffre" ref={myRef}><span style={styles.count}  id="count"><CountUp  end={number} enableScrollSpy suffix={adding} duration={5} /></span><span style={styles.noCount} id="noCount">{number}{adding}</span></span>
           <div id="label">Lorem ipsum</div> 
         </div>
       );
