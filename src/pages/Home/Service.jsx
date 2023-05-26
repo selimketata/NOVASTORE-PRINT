c55752736e52f6bb8ba036636f08a0a08bcb0285
Ahmeed-Chennaoui <ahmed.chennaoui@ensi-uma.tn>
2023-05-26 21:23:34 +0100

    Update Service.jsx

--- src/pages/Home/Service.jsx
+++ src/pages/Home/Service.jsx
@@ -1,5 +1,5 @@
-import React,{useState} from "react";
-import "./service.css";
+import React, { useState } from "react";
+import "./Service.css";
 import Abri2 from "../../assets/abri2.png";
 import imp2 from "../../assets/imp2.png";
 import plv2 from "../../assets/plv2.png";
@@ -10,31 +10,34 @@ import Img3 from "../../assets/Img3.png";
 function Service() {
   const [hover, setHover] = useState(false);
 
-   const handleMouseEnter = () => setHover(true);
-   const handleMouseLeave = () => setHover(false);
+  const handleMouseEnter = () => setHover(true);
+  const handleMouseLeave = () => setHover(false);
   return (
-   <div className="service-container"> 
-     <h1> <span>NOS</span> services </h1>
-    <hr />
-   <div className="part">
-      <div className="back-img" style={{backgroundImage:`url(${Img2}`}}>
-       <img src={Abri2} alt="FGHJK" />
-       <p>Store & Abri</p>
-      </div>
-
-       <div className="back-img" style={{backgroundImage:`url(${Img3}`}}>
-        <img src={imp2} alt="FGHJK" />
-        <p>Impression <br /> Numérique</p>
-       </div>
-
-       <div className="back-img" style={{backgroundImage:`url(${Img1}`}}>
-        <img src={plv2} alt="FGHJK" />
-       <p>PLV</p>
+    <div className="service-container">
+      <h1>
+        {" "}
+        <span>NOS</span> services{" "}
+      </h1>
+      <hr />
+      <div className="part">
+        <div className="back-img" style={{ backgroundImage: `url(${Img2}` }}>
+          <img src={Abri2} alt="FGHJK" />
+          <p>Store & Abri</p>
         </div>
-    
-   </div>
-   
- </div> 
+
+        <div className="back-img" style={{ backgroundImage: `url(${Img3}` }}>
+          <img src={imp2} alt="FGHJK" />
+          <p>
+            Impression <br /> Numérique
+          </p>
+        </div>
+
+        <div className="back-img" style={{ backgroundImage: `url(${Img1}` }}>
+          <img src={plv2} alt="FGHJK" />
+          <p>PLV</p>
+        </div>
+      </div>
+    </div>
   );
 }
 export default Service;
