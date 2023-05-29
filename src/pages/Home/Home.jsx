528e39c34104bab9b7d1402c01792274a43849f0
Ahmeed-Chennaoui <ahmed.chennaoui@ensi-uma.tn>
2023-05-29 22:58:41 +0100

    fixed css naming issues

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import "./Home.css";
-import HomeImage from '../../components/HomeImage';
+import HomeImage from "../../components/HomeImage";
 function Home() {
   return (
-    <div className="main">
-      
+    <div className="hero-main">
       <HomeImage />
-      <div className="overlay"></div>
-      <div className="content">
-      <h1>NovaStore<span>Print</span></h1>
-      <p><span>Leader</span> de la fabrication des stores et abris, les impressions numériques et les PLV.</p>
-    
+
+      <div className="hero-content">
+        <h1 className="hero-h1">
+          NovaStore<span className="hero-span">Print</span>
+        </h1>
+        <p className="hero-p">
+          <span className="hero-span">Leader</span> de la fabrication des stores
+          et abris, les impressions numériques et les PLV.
+        </p>
       </div>
-      
     </div>
   );
 }
