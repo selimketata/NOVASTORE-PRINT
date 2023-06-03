5465e4b0a325d1e6590194aec94bd66baf477626
ahmed-mrabet <121690139+ahmed-mrabet@users.noreply.github.com>
2023-06-03 05:49:12 +0100

    more in the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,18 @@
 import  {React } from 'react'
-import {Box,Typography,List, ListItemButton, ListItemText, ListItem}from"@mui/material"
+import {Box,Typography,List, ListItemButton, ListItemText, ListItem, ListItemIcon, TextField, Button}from"@mui/material"
 import Styled from"@emotion/styled"
 import "./footer.css"
 import logo from "../assets/logo.png"
+import {  FacebookRounded, Instagram, MapsUgcRounded } from '@mui/icons-material'
 
-
+const ColoredButton=Styled(Button)({
+  color:"white"
+})
 
 const StyledText=Styled(Typography)({
   display:'inline',
   fontWeight:"bold",
+  
   
 })
 function Footer() {
@@ -17,44 +21,79 @@ function Footer() {
   }
   return (<Box >
     <Box className="big">
-      <Box>
+      <Box className="one">
         <div><img src={logo} alt='logo'></img></div>
         <StyledText variant='p'sx={{fontSize:{xs:"34px",sm:"25px"}}}>NovaStore <StyledText variant='p'sx={{color:"#57C9EA"}}>Print</StyledText></StyledText>
-      <Typography>You think it </Typography>
+      <Box className="text">
+        <Typography>You think it.</Typography>
       <Typography>We print it!</Typography>
       </Box>
-      <Box>
+      
+      </Box>
+      <Box className="two">
         <Typography variant='subtitle1'>LIENS UTILES</Typography>
         <Box>
-          <List>
-          <ListItem disablePadding>
-        <ListItemButton component="a" href="/">
-        <ListItemText primary="HOME" />
+          <Box className='list'>
+          <Box className="home">
+        <ColoredButton className='links' href="/">
+        HOME 
+        </ColoredButton>
+        </Box>
+        <Box className="presentation">
+          <ColoredButton className='links'href="/presentation">
+          PRESENTATION
+         </ColoredButton>
+        </Box>
+        <Box className="services">
+        <ColoredButton className='links' href="/services">
+        SERVICES
+        </ColoredButton>
+        </Box>
+        <Box className="contact">
+        <ColoredButton className='links' href="/contacts">
+        CONTACT
+        </ColoredButton>
+        </Box>
+       </Box>
+        </Box>
+      </Box>
+      <Box className="small">
+      <Box>
+      <Typography variant='subtitle1'>NOUS CONTACTER</Typography>
+      <Typography variant='subtitle2'>+216 33 333 333</Typography>
+      <Typography variant='subtitle2'>NovaStore@gmail.com</Typography>
+      <List>
+      <ListItem disablePadding>
+        <ListItemButton  component="a" href="">
+        <ListItemIcon>
+          <FacebookRounded className='icons'/>
+        </ListItemIcon>
         </ListItemButton>
-        </ListItem>
-        <ListItem disablePadding>
-        <ListItemButton component="a" href="/presentation">
-        <ListItemText primary="PRESENTATION" />
+        <ListItemButton component="a" href="">
+        <ListItemIcon>
+          <Instagram className='icons'/>
+        </ListItemIcon>
         </ListItemButton>
-        </ListItem>
-        <ListItem disablePadding>
-        <ListItemButton component="a" href="/services">
-        <ListItemText primary="SERVICES" />
-        </ListItemButton>
-        </ListItem>
-        <ListItem disablePadding>
-        <ListItemButton component="a" href="/contacts">
-        <ListItemText primary="CONTACT" />
+        <ListItemButton component="a" href="">
+        <ListItemIcon>
+          <MapsUgcRounded className='icons'/>
+        </ListItemIcon>
         </ListItemButton>
         </ListItem>
         </List>
-        </Box>
       </Box>
-      <Box></Box>
-      <Box></Box>
+      <Box>
+      <Typography variant='subtitle1'>NEWSLETTER</Typography>
+      <Typography variant='subtitle2'>Soyer le premier a recevoir</Typography>
+      <Typography variant='subtitle2'>nos noveautés</Typography>
+      <Box>
+      <TextField type='email' label="Email" variant="standard" size="small" sx={{backgroundColor:"#F8F5EC", }}/>
+      </Box>
+      </Box>
+      </Box>
 
     </Box>
-    <Box> &copy; 2023 NOVASTORE PRINT . All Rights Reserved.</Box>
+    <Box sx={{textAlign:"center"}}> &copy; 2023 NOVASTORE PRINT . All Rights Reserved.</Box>
 
         </Box>);
 }
