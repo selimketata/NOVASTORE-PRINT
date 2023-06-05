798df399b1f548a0c4fe2c1a7c2bab0a59557655
Ahmeed-Chennaoui <ahmed.chennaoui@ensi-uma.tn>
2023-06-06 00:10:27 +0100

    fixes for avis

--- src/pages/Presentation/clients/P1.jsx
+++ src/pages/Presentation/clients/P1.jsx
@@ -1,35 +1,46 @@
-import { React} from 'react';
-import im3 from './Group 107.png';
-import pic from './pic.png'
-import './P1.css';
+import { React } from "react";
+import im3 from "./Group 107.png";
+import pic from "./pic.png";
+import "./P1.css";
 
 export default function P1(props) {
   return (
-    <div >
-      <div className='titre'>
-      <span ><h1 id='a'>Ce Que Les ClientsDisen</h1></span> <span > <h1 id='color'>t De</h1></span><span> <h1 id='b'> Nous</h1></span> </div>
+    <div>
+      <div className="avis-titre">
+        <span>
+          <h1 id="a">Ce Que Les Clients Disen</h1>
+        </span>{" "}
+        <span>
+          {" "}
+          <h1 id="color">t De</h1>
+        </span>
+        <span>
+          {" "}
+          <h1 id="b"> Nous</h1>
+        </span>{" "}
+      </div>
       <hr className="light" />
       <div className="parent">
         {props.Users &&
-         props.Users.map((record) => {
+          props.Users.map((record) => {
             return (
               <div className="av1" key={record.id}>
-                <div className="container">
+                <div className="avis-container">
                   <div className="child1">
                     <div className="appostrophe">
-                      <img width={80} src={im3}  id='m'/>
+                      <img src={im3} className="apostrophe-image" />
                     </div>
-                    <div className="text" >
+                    <div className="avis-text">
                       <p id="avis">{record.avis}</p>
                     </div>
                     <div className="imname1">
                       <div className="im">
                         <img src={record.image} id="i" />
                       </div>
-                      <div className="name">
-                        <p id="nom">{record.name}</p>
-                        <p id="p">
-                          <strong>- {record.poste} -</strong>
+                      <div className="avis-name">
+                        <p id="avis-nom">{record.name}</p>
+                        <p id="avis-p">
+                          <strong>-{record.poste}-</strong>
                         </p>
                       </div>
                     </div>
--- src/pages/Presentation/clients/P2.jsx
+++ src/pages/Presentation/clients/P2.jsx
@@ -1,6 +1,6 @@
-import P1 from './P1'
-import { useState} from 'react';
-import {  getUsers } from './Users';
+import P1 from "./P1";
+import { useState } from "react";
+import { getUsers } from "./Users";
 
 function P2() {
   const [page, setPage] = useState(1);
@@ -8,13 +8,28 @@ function P2() {
 
   return (
     <div className="App">
-      <div >
+      <div>
         <P1 Users={getUsers(page, limit)} />
       </div>
       <div className="circles">
-      <span className="dot1" onClick={() => { setPage(1)}}></span>
-      <span className="dot1" onClick={() => { setPage(2) }}></span>
-        <span className="dot3" onClick={() => { setPage(3); }}></span>
+        <span
+          className="dot1"
+          onClick={() => {
+            setPage(1);
+          }}
+        ></span>
+        <span
+          className="dot2"
+          onClick={() => {
+            setPage(2);
+          }}
+        ></span>
+        <span
+          className="dot3"
+          onClick={() => {
+            setPage(3);
+          }}
+        ></span>
       </div>
     </div>
   );
